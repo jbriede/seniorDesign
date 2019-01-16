6ab2dbc534b1eb63284c44d3e0f0940f2cd16b3f
Jacob Briede <jacob.briede@gmail.com>
2019-01-16 15:50:05 -0500

    removed some crap

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -4,22 +4,6 @@
 
 var socket = io();
 
-
-// var item0 = {"id": 0, 
-// 			"name": "Lemonade",
-// 			 "available": true};
-
-
-
-// var item1 = {"id": 1, "name": "Iced Tea", "available": true};
-// var item2 = {"id": 2, "name": "Coke", "available": true};
-// var item3 = {"id": 3, "name": "Sprite", "available": true};
-// var item4 = {"id": 4, "name": "Rum", "available": true};
-// var tankArray = [item0, item1, item2, item3, item4];
-
-
-
-
 loadCombinationsProcedure(socket);
 loadTanks(socket);
 
@@ -28,7 +12,6 @@ addDrinkButton.onclick = function()
 {
 	addDrinkProcedure(socket).then(function(result) 
 	{
-
 		/* Once the new drink has been sent to the server, the promise will resolve */
 		loadCombinationsProcedure(socket);
 	}, function(err) 
@@ -55,11 +38,6 @@ var setTemperatureButton = document.getElementById("setTemperature");
 setTemperatureButton.onclick = function()
 {
 	tempSetProcedure(socket);
-	// socket.emit("getTemperature");
-	// socket.on("tempReturn", function(newTemp)
-	// {
-	// 	console.log("Server says temp is :", newTemp);
-	// });
 }
 var currTempButton = document.getElementById("currTempButton");
 
