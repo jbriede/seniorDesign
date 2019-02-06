33ff09264a0824c65b3e9541388dcff880c15bc0
Jacob Briede <jacob.briede@gmail.com>
2019-02-06 14:09:16 -0500

    starting to break up css and html

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -3,66 +3,71 @@
  */
 
 var socket = io();
+/* I hate jquery but I need it for this i guess. */
+$("#menuContainer").load('HTML/menu.html'); 
+var menuContainer = document.getElementById("menuContainer");
 
-loadCombinationsProcedure(socket);
-loadTanks(socket);
 
-var addDrinkButton = document.getElementById("addDrinkButton");
-addDrinkButton.onclick = function()
-{
-	addDrinkProcedure(socket).then(function(result) 
-	{
-		/* Once the new drink has been sent to the server, the promise will resolve */
-		loadCombinationsProcedure(socket);
-	}, function(err) 
-	{
-	  console.log(err); // Error: "It broke"
-	});
-}
 
-var deleteDrinkButton = document.getElementById("deleteDrinkButton");
-deleteDrinkButton.onclick = function()
-{
-	deleteDrinkProcedure(socket).then(function(result) 
-	{
+// loadCombinationsProcedure(socket);
+// loadTanks(socket);
 
-		/* Once the new drink has been sent to the server, the promise will resolve */
-		loadCombinationsProcedure(socket);
-	}, function(err) 
-	{
-	  console.log(err); // Error: "It broke"
-	});
-}
+// var addDrinkButton = document.getElementById("addDrinkButton");
+// addDrinkButton.onclick = function()
+// {
+// 	addDrinkProcedure(socket).then(function(result) 
+// 	{
+// 		/* Once the new drink has been sent to the server, the promise will resolve */
+// 		loadCombinationsProcedure(socket);
+// 	}, function(err) 
+// 	{
+// 	  console.log(err); // Error: "It broke"
+// 	});
+// }
 
-var setTemperatureButton = document.getElementById("setTemperature");
-setTemperatureButton.onclick = function()
-{
-	tempSetProcedure(socket);
-}
-var currTempButton = document.getElementById("currTempButton");
+// var deleteDrinkButton = document.getElementById("deleteDrinkButton");
+// deleteDrinkButton.onclick = function()
+// {
+// 	deleteDrinkProcedure(socket).then(function(result) 
+// 	{
 
-var tempCallback = function()
-{
-	setTimeout(function()
-	{ 
-		getTemp(socket).then(function(temp) 
-		{
+// 		/* Once the new drink has been sent to the server, the promise will resolve */
+// 		loadCombinationsProcedure(socket);
+// 	}, function(err) 
+// 	{
+// 	  console.log(err); // Error: "It broke"
+// 	});
+// }
 
-			/* Once the new drink has been sent to the server, the promise will resolve */
-			currTempButton.innerHTML = "(" + temp + " F)";
-			tempCallback();
-		}, function(err) 
-		{
-		  console.log(err); // Error: "It broke"
-		});
+// var setTemperatureButton = document.getElementById("setTemperature");
+// setTemperatureButton.onclick = function()
+// {
+// 	tempSetProcedure(socket);
+// }
+// var currTempButton = document.getElementById("currTempButton");
 
-	}, 3000);
-}
-tempCallback();
+// var tempCallback = function()
+// {
+// 	setTimeout(function()
+// 	{ 
+// 		getTemp(socket).then(function(temp) 
+// 		{
 
-var fillTankButton = document.getElementById("fillTankButton");
-fillTankButton.onclick = function()
-{
+// 			/* Once the new drink has been sent to the server, the promise will resolve */
+// 			currTempButton.innerHTML = "(" + temp + " F)";
+// 			tempCallback();
+// 		}, function(err) 
+// 		{
+// 		  console.log(err); // Error: "It broke"
+// 		});
 
-	FillTankProcedure(socket); 
-}
+// 	}, 3000);
+// }
+// tempCallback();
+
+// var fillTankButton = document.getElementById("fillTankButton");
+// fillTankButton.onclick = function()
+// {
+
+// 	FillTankProcedure(socket); 
+// }
