f8d70d5950b0ab07c10348a9ab1dcfb95f01220b
Your Name <you@example.com>
2019-04-12 18:46:07 -0400

    fits window somewhat

--- window.js
+++ window.js
@@ -3,8 +3,9 @@ const {app, BrowserWindow} = require('electron')
 
 app.on('ready', function()
 {
-	let win = new BrowserWindow({width: 800, height: 480, frame:false, }); 
+	let win = new BrowserWindow({width: 800, height: 480, frame:false, zoomToPageWidth: true }); 
 	win.setMenu(null);
+	//win.setFullScreen(true);
 
 	win.loadURL('http://localhost:3000');
 });
